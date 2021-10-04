06b11f3a5049bede8d36a1ce693d1de2a3c41fb0
sagarkoju <sagarkoju5@gmail.com>
2021-10-04 21:43:53 +0545

    enum practise

---
+++ enum.dart
@@ -0,0 +1,28 @@
+enum name { first, second, third, fourth, fifth }
+
+void main() {
+  print(name.values);
+  print(name.fifth.index);
+  name.values.forEach((e) => print("name $e"));
+  print(name.first.runtimeType);
+  String typename = type(name.first);
+  print(typename);
+  for (var x in name.values) {
+    print(x);
+  }
+}
+
+String type(name e) {
+  switch (e) {
+    case name.first:
+      return "First";
+    case name.second:
+      return "Second";
+    case name.third:
+      return "Third";
+    case name.fourth:
+      return "Fourth";
+    case name.fifth:
+      return "Fifth";
+  }
+}
