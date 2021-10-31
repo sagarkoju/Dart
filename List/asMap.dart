09125d5e70895490b70d0231732bea8b5fed714e
sagarkoju <sagarkoju5@gmail.com>
2021-10-31 19:50:11 +0545

    asMap modified

--- List/asMap.dart
+++ List/asMap.dart
@@ -2,12 +2,14 @@
 void main() {
   List<String> words = ['fee', 'fi', 'fo', 'fum'];
   Map<int, String> map = words.asMap();
+  var s = map.entries.map((e) => e).toList();
   var ss = (map[0] as String) + (map[1] as String); // 'feefi';
-  var s = map.entries.map((e) => e);
   var sss = map.values.toList();
-  var ssss = map.keys.toList(); // [0, 1, 2, 3]
+  var ssss = map.entries.first;
+  var sssss = map.keys.toList(); // [0, 1, 2, 3]
   print(s);
   print(ss);
   print(sss);
   print(ssss);
+  print(sssss);
 }
