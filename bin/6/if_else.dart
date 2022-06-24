feb5dee5b9bd8b68f50b87f816d7902e0b41872e
sagarkoju <sagarkoju5@gmail.com>
2022-06-24 15:47:19 +0545

    initial commit

---
+++ bin/6/if_else.dart
@@ -0,0 +1,12 @@
+void main() { 
+   var num = 2; 
+   if(num > 0) { 
+      print("${num} is positive"); 
+   } 
+   else if(num < 0) { 
+      print("${num} is negative"); 
+   } else { 
+      print("${num} is neither positive nor negative"); 
+   }
+   
+}
